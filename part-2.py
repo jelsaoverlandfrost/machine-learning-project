3d02fea4232df67245e98e9c5284bd3063cc3b13
JelsaOverlandFrost <JelsaOverlandFrost@Gmail.com>
2018-12-08 12:46:07 +0800

    Commit

--- part-2.py
+++ part-2.py
@@ -1,18 +1,115 @@
-# TODO: Write a parser for the input files
-# For better referencing:
-SENTIMENT_TAGS = {}
-CHUNK_TAGS = {}
+import argparse
 
 
-# This function is for reading the input files
-def parse_input_files(file_name):
-    result = []
-    with open(file_name, 'r') as file:
-        while True:
-            data_read = file.readline()
-            if data_read == '':
-                break
-            print(data_read.replace('\n', ''))
+def parse_training_file(file_name):
+    print('Parsing training file...')
+    training_file = open(file_name, 'r', encoding='utf-8')
+    list_words = []
+    list_tags = []
+    for line in training_file:
+        if line != '' and line != '\n':
+            content = line.split()
+            word = line[:(len(line) - len(content[-1])) - 2]
+            tag = content[-1]
+            list_words.append(word)
+            list_tags.append(tag)
+    print('Parsing finished.')
+    return list_words, list_tags
 
 
-parse_input_files("data\\EN\\train")
+def parse_test_file(filename):
+    print('Parsing testing file...')
+    testing_file = open(filename, 'r', encoding='utf-8')
+    list_words = []
+    list_current_sentence = []
+    for line in testing_file:
+        if line != '\n' and line != '':
+            list_current_sentence.append(line.split()[0])
+        elif line == '\n':
+            list_words.append(list_current_sentence)
+        else:
+            break
+    print('Parsing finished.')
+    return list_words
+
+
+# This function estimates the emission parameter from the training set using MLE
+# list_tags = nested list of sequences of y sequences
+# list_words = nested list of sequences of x sequences
+# Assume valid sequence
+def train_emission(list_words, list_tags):
+    print('Calculating Emission...')
+    dictionary_words = {}
+    dictionary_tags = {}
+    for i in range(len(list_tags)):
+        if list_words[i] not in dictionary_words:
+            new_pair = {list_tags[i]: 1.0}
+            dictionary_words[list_words[i]] = new_pair
+        if list_tags[i] not in dictionary_words[list_words[i]]:
+            dictionary_words[list_words[i]][list_tags[i]] = 1.0
+        else:
+            dictionary_words[list_words[i]][list_tags[i]] = dictionary_words[list_words[i]][list_tags[i]] + 1
+        if list_tags[i] not in dictionary_tags:
+            dictionary_tags[list_tags[i]] = 1.0
+        else:
+            dictionary_tags[list_tags[i]] = dictionary_tags[list_tags[i]] + 1.0
+    print('Finished.')
+    return dictionary_words, dictionary_tags
+
+
+def test_emission(list_tags, list_words, tag, word, k):
+    word_counter = 0.0
+    tag_counter = 0.0
+    for i in range(len(list_tags)):
+        if (list_words[i] == word) and (list_tags[i] == tag):
+            word_counter = word_counter + 1
+        if i == tag:
+            tag_counter = tag_counter + 1
+    if word_counter != 0:
+        return float(word_counter) / (tag_counter + k)
+    else:
+        return k / (tag_counter + k)
+
+
+def argmax(list_tags, list_words, x, k, tags):
+    max_arg = 0
+    tag = ""
+    for i in tags:
+        if test_emission(list_tags, list_words, i, x, k) > max_arg:
+            max_arg = test_emission(list_tags, list_words, i, x, k)
+            tag = i
+    return tag, max_arg
+
+
+def count_unknown(list_words, list_test_seq):
+    count = 0
+    for seq in list_test_seq:
+        for word in seq:
+            if word not in list_words:
+                count = count + 1
+    return count
+
+
+def part_2(test_file_name, train_file_name, result_file_name):
+    try:
+        result_file = open(result_file_name, "w+", encoding="utf-8")
+        list_words, list_tags = parse_training_file(train_file_name)
+        list_test_seq = parse_test_file(test_file_name)
+        dict_words, dict_tags = train_emission(list_words, list_tags)
+        predict = {}
+        number = 0
+        k = count_unknown(list_words, list_test_seq)
+        for seq in list_test_seq:
+            print('check')
+            for word in seq:
+                if word not in predict:
+                    tag, max_arg = argmax(list_tags, list_words, word, k, dict_tags.keys())
+                    predict[word] = tag
+                result_file.write(word + " " + predict[word] + "\n")
+            number = number + 1
+            result_file.write('\n')
+    finally:
+        result_file.close()
+
+
+part_2('data/SG/dev.in', 'data/SG/train', 'data/SG/dev.p2.out')
---
+++ part-3.py
@@ -0,0 +1,347 @@
+###Part 3
+
+def protoemission(listy, listx):
+    print(listy)
+    print(listx)
+    dictionary = {}
+    ydictionary = {}
+    for i in range(len(listy)):
+        if listx[i] not in dictionary:
+            #            dictionary[listx[i]][listy[i]]=1.0
+            newdic = {}
+            newdic[listy[i]] = 1.0
+            dictionary[listx[i]] = newdic
+        if listy[i] not in dictionary[listx[i]]:
+            dictionary[listx[i]][listy[i]] = 1.0
+        else:
+            dictionary[listx[i]][listy[i]] = dictionary[listx[i]][listy[i]] + 1
+        if listy[i] not in ydictionary:
+            ydictionary[listy[i]] = 1.0
+        else:
+            ydictionary[listy[i]] = ydictionary[listy[i]] + 1.0
+
+    return dictionary, ydictionary
+
+
+#    print(y,counter,intercount(listy,y)+k)
+
+def finalemission(dic, ydic, x, y, k):
+    if x not in dic:
+        return k / (ydic[y] + k)
+    else:
+        if y not in dic[x]:
+            return 0
+        else:
+            return dic[x][y] / (ydic[y] + k)
+
+
+def testemission(listy, listx, y, x, k):
+    counter = 0.0
+    ycounter = 0.0
+    for i in range(len(listy)):
+        if (listx[i] == x) & (listy[i] == y):
+            counter = counter + 1
+        if (i == y):
+            ycounter = ycounter + 1
+    #    print(y,counter,intercount(listy,y)+k)
+    if counter != 0:
+        return float(counter) / (ycounter + k)
+    else:
+        return k / (ycounter + k)
+
+
+def argmaxx(listy, listx, x, k, rangey):
+    #    rangey=["O","B-positive","B-neutral","B-negative","I-positive","I-neutral","I-negative"]
+    maxarg = 0
+    tag = ""
+    for i in rangey:
+        #        print(i,testemission(listy,listx,i,x,k))
+        if testemission(listy, listx, i, x, k) > maxarg:
+            maxarg = testemission(listy, listx, i, x, k)
+            tag = i
+    return tag, maxarg
+
+
+def settle(filename):
+    print("Start with training")
+    file = open(filename, "r", encoding="UTF-8")
+    listy = []
+    listy = []
+    listx = []
+    for line in file:
+        listline = line.split()
+        if len(listline) != 0:
+            listx.append(line[:(len(line) - len(listline[-1])) - 2])
+            #            listx.append(listline[0].lower())
+            listy.append(listline[-1])
+    print("Done with training")
+    #    print(listx)
+    return listx, listy
+
+
+def testproto(testname, trainname, result, k):
+    try:
+        testfile = open(testname, "r", encoding="UTF-8")
+        resultfile = open(result, "w+", encoding="UTF-8")
+        listx, listy = settle(trainname)
+        predict = {}
+        number = 0
+        for word in testfile:
+            listword = word.split()
+            if (len(listword) == 1):
+                #                newword=listword[0].lower()
+                #                if (newword not in predict):
+                if (listword[0] not in predict):
+                    (key, value) = argmaxx(listy, listx, listword[0], k)
+                    predict[listword[0]] = key
+                resultfile.write(listword[0] + " " + predict[listword[0]] + "\n")
+            #                    (key,value)=argmaxx(listy,listx,newword,k)
+            #                    predict[newword]=key
+            #                resultfile.write(listword[0]+" "+predict[newword]+"\n")
+            else:
+                resultfile.write("\n")
+            number = number + 1
+            print(number)
+    #        print(predict)
+    finally:
+        resultfile.close()
+
+
+def testword(word, trainname, k):
+    listx, listy = settle(trainname)
+    #    print (intercount(listy,"O"))
+    #    print(word.lower())
+    #    print(argmaxx(listy,listx,word.lower(),k))
+    return 0
+
+
+def unseennumber(testname, train):
+    test = open(testname, "r", encoding="UTF-8")
+    listx, listy = settle(train)
+    counter = 0
+    for word in test:
+        listword = word.split()
+        if (len(listword) == 1):
+            if (listword[0].lower() not in listx):
+                counter = counter + 1
+    return counter
+
+
+def transition(listy, first, second):
+    check = False
+    numerator = 0.0
+    denominator = 0.0
+    if (first == ""):
+        check = True
+        denominator = 1
+    for line in listy:
+        if ((line == second) & check):
+            numerator = numerator + 1
+        #            print(check,numerator)
+        check = False
+
+        if (line == first):
+            check = True
+            denominator = denominator + 1
+    #            print(check,denominator)
+
+    if (second == ""):
+        numerator = numerator + 1
+
+    if (denominator != 0):
+        return numerator / denominator
+    else:
+        return 0
+
+
+def transitionlist(listy, rangexy):
+    dictionary = {}
+    rangey = rangexy.copy()
+    rangey.append("")
+    for i in range(len(rangey)):
+        anotherdictionary = {}
+        for j in range(len(rangey)):
+            if ((i + j != 2 * len(rangey))):
+                anotherdictionary[rangey[j]] = transition(listy, rangey[i], rangey[j])
+        dictionary[rangey[i]] = anotherdictionary
+    return dictionary
+
+
+def handle(filename):
+    print("Start with y")
+    file = open(filename, "r", encoding="UTF-8")
+    listy = []
+    for line in file:
+        listline = line.split();
+        if (len(listline) != 0):
+            listy.append(listline[-1])
+        else:
+            listy.append("")
+    print("Done with y")
+    return listy
+
+
+# def argmaxy(listy,y):
+#    rangey = ["O","B-positive","B-neutral","B-negative","I-positive","I-neutral","I-negative",""]
+#    maxarg = 0
+#    tag = ""
+#    for word in rangey:
+#        if (transition(listy,y,word)>maxarg):
+#            maxarg = transition(listy,y,word)
+#            tag = word
+#    return (tag,maxarg)
+
+
+# listy=handle(train)
+# print(transition(listy,"B-negative","O"))
+# print(argmaxy(listy,"B-positive"))
+
+
+def extractx(filename):
+    print("Start with x")
+    file = open(filename, "r", encoding="UTF-8")
+    listx = []
+    for line in file:
+        listline = line.split();
+        if (len(listline) != 0):
+            listx.append(line[:-1])
+        #            listx.append(listline[0].lower())
+        else:
+            listx.append("")
+
+    print("Done with x")
+    return listx
+
+
+class node:
+    def __init__(self, prob, parent):
+        self.prob = prob
+        self.parent = parent
+
+
+# class prob:
+#    def __init__(self,number,magnitude):
+#        self.number=number
+#        self.mag=magnitude
+#    def multiply(self,other.)
+def ytag(trainname):
+    file = open(trainname, "r", encoding="UTF-8")
+    listy = []
+    for line in file:
+        listline = line.split();
+        if (len(listline) == 2):
+            if (listline[1] not in listy):
+                listy.append(listline[1])
+    return listy
+
+
+def viterbi(testname, trainname, resultname, k):
+    #    Initialization
+
+    try:
+        resultfile = open(resultname, "w+", encoding="UTF-8")
+        current = []
+        prob = []
+        emissionx, emissiony = settle(trainname)
+        edic, ydic = protoemission(emissiony, emissionx)
+        x = extractx(testname)
+        rangey = ytag(trainname)
+        ydictionary = transitionlist(handle(trainname), rangey)
+        where = 0;
+        for wordie in x:
+            # Initialise newlist to store prob
+            where = where + 1
+            if (where % 100 == 0):
+                print(str(where))
+            #            print(current)
+            newlist = []
+            argmax = 0
+            tag = 0
+            # if start of sentence
+            if (len(prob) == 0):
+                for y in rangey:
+                    probability = ydictionary[""][y]
+                    word = node(probability, 7)
+                    newlist.append(word)
+                prob.append(newlist)
+
+            # if not start of sentence or end of sentence
+            elif (wordie != ""):
+                latestprob = prob[len(prob) - 1]
+                for i in range(len(rangey)):
+                    argmax = 0
+                    for j in range(len(rangey)):
+                        initialprob = latestprob[j].prob
+                        transitionprob = ydictionary[rangey[j]][rangey[i]]
+                        emissionprob = finalemission(edic, ydic, wordie, rangey[i], k)
+                        #                        emissionprob = finalemission(edic,ydic,wordie.lower(),rangey[i],k)
+                        #                        print(initialprob,transitionprob,emissionprob,argmax)
+                        a = initialprob * transitionprob * emissionprob
+                        if (a > argmax):
+                            argmax = a
+                            tag = j
+                    word = node(argmax, tag)
+                    #                    print(argmax,tag)
+                    newlist.append(word)
+                prob.append(newlist)
+                # If end of sentence
+            else:
+                for j in range(len(rangey)):
+                    initialprob = prob[len(prob) - 1][j].prob
+                    transitionprob = ydictionary[rangey[j]][""]
+                    a = initialprob * transitionprob
+                    if (a > argmax):
+                        argmax = a
+                        tag = j
+                currentpointer = tag
+                position = -1
+                order = []
+                #                print(position,currentpointer)
+                #                print(len(prob))
+                #
+                #                for i in prob:
+                #                    print("!")
+                #                    for j in i:
+                #                        print(j.prob)
+
+                #                print(current)
+                while (position != ((-1 * len(prob)) - 1)):
+                    order.append(currentpointer)
+                    currentpointer = prob[position][currentpointer].parent
+                    position = position - 1
+                #                print(len(current),len(order))
+                #                print(order)
+                for i in range(len(order)):
+                    resultfile.write(str(current[i]) + " " + rangey[order[(-1 * i) - 1]])
+                    resultfile.write("\n")
+                resultfile.write("\n")
+            if (wordie != ""):
+                current.append(wordie)
+            else:
+                newlist = []
+                current = []
+                prob = []
+        # Out of loop
+        #        for j in range(7):
+        #            initialprob = prob[len(prob)-1][j].prob
+        #            transitionprob = ydictionary[rangey[j]][""]
+        #            if (initialprob*transitionprob>argmax):
+        #                argmax=initialprob*transitionprob
+        #                tag=j
+        #        currentpointer=tag
+        #        position=-1
+        #        order=[]
+        #                print(position,currentpointer)
+        #        if (len(prob)!=0):
+        #            while (position != ((-1*len(prob))-1)):
+        #                order.append(currentpointer)
+        #                currentpointer=prob[position][currentpointer].parent
+        #                position=position-1
+        ##                    print(len(current),len(order))
+        ##                    print(order)
+        #            for i in range(len(order)):
+        #                resultfile.write(str(current[i])+" "+rangey[order[(-1*i)-1]])
+        #                resultfile.write("\n")
+        resultfile.write("\n")
+    finally:
+        resultfile.close()
